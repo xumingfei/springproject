ca0d42f6c5a879609bfbfc24d95934f8da45d799
xumingfei <18832892030@163.com>
2020-04-18 08:49:35 +0800

    修改springboot集成redis项目名

---
+++ springboot-redis/src/test/java/com/practice/springbootredis/SpringbootRedisApplicationTests.java
@@ -0,0 +1,31 @@
+package com.practice.springbootredis;
+
+import com.practice.springbootredis.entity.User;
+import com.practice.springbootredis.service.RedisService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootRedisApplicationTests {
+
+    @Autowired
+    RedisService redisService;
+    @Test
+    public void contextLoads() {
+    }
+
+    @org.junit.Test
+    public void setObject(){
+        redisService.setObj("a",new User("张三",20));
+    }
+
+    @org.junit.Test
+    public void getObject(){
+        System.out.println(redisService.getObject("a"));
+    }
+
+}
